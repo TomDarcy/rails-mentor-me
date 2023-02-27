02f53359e403cc1a1cc2df978ee1ddcb3cf83dec
Tom Darcy <tdarcytom@googlemail.com>
2023-02-27 17:48:39 +0800

    end of day

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  recources :mentors 
+  resources :mentors
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :bookings, only: [:index, :new, :create, :edit, :show, :destroy] do
     resources :bookmarks, only: [:create]
