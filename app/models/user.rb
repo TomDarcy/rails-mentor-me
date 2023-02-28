67b8555e5fe36c631e6b8ae8302f3c2991321eea
Aigerim Karataeva <ai.gerim@hotmail.com>
2023-02-28 15:11:07 +0800

    new registration controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  # after_save :create_a_mentor
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +7,13 @@ class User < ApplicationRecord
 
   has_one_attached :photo
   has_many :bookings
+
+  def create_a_mentor
+    not_exist = Mentor.find_by_user_id(id).nil?
+    if mentor? && not_exist
+      @mentor = Mentor.create(user_id: id)
+    end
+    # go to mentors edit page
+    # redirect_to edit_mentor_path(@mentor.id)
+  end
 end
