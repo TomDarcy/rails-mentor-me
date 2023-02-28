8762ffb7ec322386967096f67243b92c4d48d6b3
Aigerim Karataeva <ai.gerim@hotmail.com>
2023-02-28 15:37:20 +0800

    index photos

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,12 @@ class User < ApplicationRecord
   has_one_attached :photo
   has_many :bookings
 
-  def create_a_mentor
-    not_exist = Mentor.find_by_user_id(id).nil?
-    if mentor? && not_exist
-      @mentor = Mentor.create(user_id: id)
-    end
-    # go to mentors edit page
-    # redirect_to edit_mentor_path(@mentor.id)
-  end
+  # def create_a_mentor
+  #   not_exist = Mentor.find_by_user_id(id).nil?
+  #   if mentor? && not_exist
+  #     @mentor = Mentor.create(user_id: id)
+  #   end
+  #   # go to mentors edit page
+  #   # redirect_to edit_mentor_path(@mentor.id)
+  # end
 end
