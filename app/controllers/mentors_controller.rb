307673760ec9c441ca338c6198542842dcc8979a
Aigerim Karataeva <ai.gerim@hotmail.com>
2023-02-28 12:32:20 +0800

    picture

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,10 +1,12 @@
 class MentorsController < ApplicationController
-<<<<<<< HEAD
-
-=======
->>>>>>> 3512c57f58bd5cd9265743bb9f79e91a9ca36881
   def index
     @mentors = Mentor.all
+    @mentors = @mentors.where(country: params[:country]) if params[:country].present?
+    # @countries = @mentors.pluck(:country).uniq
+    @mentors = @mentors.where(field: params[:field]) if params[:field].present?
+    # @fields = @mentors.pluck(:field).uniq
+    @mentors = @mentors.where("experience >= ?", params[:experience]) if params[:experience].present?
+    @mentors = @mentors.where("price_per_hour <= ?", params[:price_per_hour]) if params[:price_per_hour].present?
   end
 
   def show
