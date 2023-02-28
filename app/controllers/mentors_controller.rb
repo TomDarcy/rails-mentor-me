0346955717aa25b122eb6ffeeb5f532d2055fb78
Tom Darcy <tdarcytom@googlemail.com>
2023-02-28 12:33:41 +0800

    show page ready to go for wed release

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -5,5 +5,24 @@ class MentorsController < ApplicationController
 
   def show
     @mentor = Mentor.find(params[:id])
+    @booking = Booking.new
+  end
+
+  def create_booking
+    @mentor = Mentor.find(params[:id])
+    @booking = Booking.new(booking_params)
+    @booking.mentor = @mentor
+
+    if @booking.save
+      redirect_to @mentor, notice: 'Booking was successfully created.'
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time, :description)
   end
 end
